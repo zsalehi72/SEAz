8bc909786eb4da88d8b00b6563881a8aaec5fe9b
z_salehi <z.salehi.m90@gmail.com>
2015-02-19 10:14:04 +0330

    Test file chenged.

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,9 @@ class Test
     public static void main(String[] args)
     {
         testSum();
+		testSubtract();
+		testMultiply();
+		testDivide();
     }
 
     private static void testSum()
@@ -21,4 +24,72 @@ class Test
             System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
         }
     }
+	
+	private static void testSum()
+	{
+		int a = 5;
+		int b = 6;
+		int expected = 11;
+		BasicMath bm = new BasicMath();
+		int result = bm.sum(a, b);
+		if (result == expected)
+		{
+			System.out.println("Sum: OK");
+		}
+		else
+		{
+			System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
+		}
+	}
+	
+	private static void testSubtract()
+	{
+		int a = 5;
+		int b = 6;
+		int expected = -1;
+		BasicMath bm = new BasicMath();
+		int result = bm.subtract(a, b);
+		if (result == expected)
+		{
+			System.out.println("Subtract: OK");
+		}
+		else
+		{
+			System.out.println("Subtract: Failed, expected = " + expected + ", result = " + result);
+		}
+	}
+		
+	private static void testMultiply()
+	{
+		int a = 5;
+		int b = 6;
+		int expected = 30;
+		BasicMath bm = new BasicMath();
+		int result = bm.multiply(a, b);
+		if (result == expected)
+		{
+			System.out.println("Multiply: OK");
+		}
+		else
+		{
+			System.out.println("Multiply: Failed, expected = " + expected + ", result = " + result);
+		}
+	}
+		
+	private static void testDivide()
+	{
+		int a = 10;
+		int b = 5;
+		int expected = 2;
+		BasicMath bm = new BasicMath();
+		int result = bm.divide(a, b);
+		if (result == expected)
+		{
+			System.out.println("Divide: OK");
+		}
+		else
+		{
+			System.out.println("Divide: Failed, expected = " + expected + ", result = " + result);
+		}
+	}
 }
